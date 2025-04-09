bfab7e6be8890cf03f0aaeb2bf941e64c54f4fff
Sergio <gomez.c.sergio@outlook.com>
2025-04-09 14:47:57 +0200

    Add Price Calculation

---
+++ BLLAccountingDemo/WDayManager.cs
@@ -0,0 +1,57 @@
+﻿using EFAccounting;
+using EFAccounting.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BLLAccountingDemo
+{
+    public class WDayManager
+    {
+        private Context _context;
+
+        public WDayManager(Context context)
+        {
+            _context = context;
+        }
+
+        public void AddWDay(WDay wd)
+        {
+            _context.Wdays.Add(wd);
+            _context.SaveChanges();
+        }
+
+        public void UpdateWDay(WDay wday) 
+        {
+            _context.Wdays
+                .Where(wd => wd.Id == wday.Id)
+                .ExecuteUpdate(u => u
+                    .SetProperty(p => p.Kid, wday.Kid)
+                    .SetProperty(p => p.Price, wday.Price)
+                    .SetProperty(p => p.Arrival, wday.Arrival)
+                    .SetProperty(p => p.Departure, wday.Departure)
+                    .SetProperty(p => p.Date, wday.Date)
+                );
+            _context.SaveChanges();
+        }
+
+        public void DeleteWDay(WDay wDay)
+        {
+            _context.Wdays.Where(w => w.Id == wDay.Id).ExecuteDelete();
+            _context.SaveChanges();
+        }
+
+
+        public float CalculateAmount(DateOnly StartDate, DateOnly EndDate, List<Kid> Kids)
+        {
+            float amount = 0f;
+
+            List<WDay> wdays = _context.Wdays.Where(wd => Kids.Contains(wd.Kid) && wd.Date >= StartDate && wd.Date <= EndDate).ToList();
+            foreach (WDay wday in wdays) 
+            {
+                TimeSpan WdayHours = (TimeOnly)wday.Departure! - (TimeOnly)wday.Arrival!;
+                amount += (float)WdayHours.TotalHours * wday.Price.Value;
+            }
+
+            return amount;
+        }
+    }
+}
