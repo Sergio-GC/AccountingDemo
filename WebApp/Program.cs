7205eeb06db124fc8fdcfe2b9b8f1622cd25eae3
Sergio <gomez.c.sergio@outlook.com>
2025-07-24 15:52:27 +0200

    Set up localization

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+using System.Reflection;
 using WebApp.Controllers;
+using WebApp.src;
 
 namespace WebApp
 {
@@ -12,6 +17,41 @@ namespace WebApp
             builder.Services.AddControllersWithViews();
             builder.Services.AddHttpClient<HomeController>();
 
+            // Add localization
+            builder.Services.AddSingleton<LocaleService>();
+            builder.Services.AddLocalization(options => options.ResourcesPath = "src");
+
+            builder.Services.Configure<RequestLocalizationOptions>(opt =>
+            {
+                var supportedCultures = new List<CultureInfo>
+                {
+                    new CultureInfo("en-US"),
+                    new CultureInfo("es-ES"),
+                    new CultureInfo("fr-CH")
+                };
+
+                opt.DefaultRequestCulture = new RequestCulture("en-US");
+                opt.SupportedCultures = supportedCultures;
+                opt.SupportedUICultures = supportedCultures;
+
+                // opt.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+                opt.RequestCultureProviders = new List<IRequestCultureProvider>
+                {
+                    new CookieRequestCultureProvider()
+                };
+            });
+
+            builder.Services.AddMvc()
+                .AddViewLocalization()
+                .AddDataAnnotationsLocalization(opt =>
+                {
+                    opt.DataAnnotationLocalizerProvider = (type, factory) =>
+                    {
+                        var assemblyName = new AssemblyName(typeof(SharedLocale).Assembly.FullName);
+                        return factory.Create("SharedLocale", assemblyName.Name);
+                    };
+                });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -21,6 +61,9 @@ namespace WebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            var locOptions = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(locOptions.Value);
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
---
+++ WebApp/src/LocaleService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Reflection;
+
+namespace WebApp.src
+{
+    public class LocaleService
+    {
+        private readonly IStringLocalizer _stringLocalizer;
+
+        public LocaleService(IStringLocalizerFactory factory)
+        {
+            var type = typeof(SharedLocale);
+            var assemblyName = new AssemblyName(type.Assembly.FullName);
+
+            _stringLocalizer = factory.Create("SharedLocale", assemblyName.Name);
+        }
+
+        public LocalizedString GetLocalizedString(string key)
+        {
+            return _stringLocalizer[key];
+        }
+    }
+}
