44282f63a6db8f3375308606c6c385dd6d53767a
Sergio <gomez.c.sergio@outlook.com>
2025-07-24 23:52:12 +0200

    Create Kids list view

---
+++ WebApp/Controllers/KidsController.cs
@@ -0,0 +1,33 @@
+﻿using DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class KidsController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
+        private readonly string _baseUrl;
+
+        public KidsController(ILogger<HomeController> logger, HttpClient httpClient, IConfiguration configuration)
+        {
+            _logger = logger;
+            _httpClient = httpClient;
+            _baseUrl = configuration["ApiBaseUrl"]!;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Kids()
+        {
+            List<Kid> kids = new List<Kid>();
+            kids = await _httpClient.GetFromJsonAsync<List<Kid>>(_baseUrl + "kids");
+
+            return View(kids);
+        }
+    }
+}
